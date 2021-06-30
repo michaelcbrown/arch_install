29628b046a5bcaf3145aa70ef52f6aba06ef2253
michaelcbrown <55893579+michaelcbrown@users.noreply.github.com>
2021-06-30 15:04:21 -0400

    Update post_reboot.sh
    fixing basic errors that popped up first time I used it

--- post_reboot.sh
+++ post_reboot.sh
@@ -4,9 +4,10 @@ sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R mb:mb ./yay-git
 cd yay-git
 makepkg -si
-sudo pacman -S pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server xterm
-sudo pacman -S feh bspwm sxhkd maim xclip picon ttf-font awesome rofi
-yay -S polybar xst
+sudo pacman -S pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server xterm --noconfirm
+sudo pacman -S feh bspwm sxhkd maim xclip picon ttf-font awesome rofi --noconfirm
+yay -S polybar xst --noconfirm
+mkdir ~/.config
 mkdir ~/.config/bspwm
 mkdir ~/.config/sxhkd
 ln -sf ~/builds/bspwm_dotfiles/bspwmrc ~/.config/bspwm
@@ -16,17 +17,18 @@ ln -sf ~/builds/bspwm_dotfiles/picom ~/.config/picom
 ln -sf ~/builds/bspwm_dotfiles/polybar ~/.config/polybar
 ln -sf ~/builds/bspwm_dotfiles/rofi ~/.config/rofi
 ln -sf ~/builds/bspwm_dotfiles/Xresources ~/.Xresources
+cd ~/builds
 git clone https://github.com/siduck76/bspwm-dotfiles
 mkdir ~/.local
 mkdir ~/.local/share
 mkdir ~/.local/share/fonts
-cp ~/builds/bspwm_dotfiles/fonts\!\ \(\ jetbrainsmono\ nerd\ font\ +\ material/ /) ~/.local/share/fonts
+cp ~/builds/bspwm-dotfiles/fonts\!\ \(\ jetbrainsmono\ nerd\ font\ +\ material\ \) ~/.local/share/fonts
 cd ~/.local/share/fonts
 fc-cache -fv
 cd ~/builds/bspwm_dotfiles
 make && sudo make install
 ### can I run xrdb merge .Xresources here? or will it be an error
-sudo pacman -S zsh
+sudo pacman -S zsh --noconfirm
 chsh -s /usr/bin/zsh
-sudo pacman -S papirus-icon-theme
-yay -S canta-gtk-theme
+sudo pacman -S papirus-icon-theme --noconfirm
+yay -S canta-gtk-theme --noconfirm
