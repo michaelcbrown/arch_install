f9b79edf19eb7b43ab599c8e490aee022d868c24
michaelcbrown <55893579+michaelcbrown@users.noreply.github.com>
2021-06-30 16:38:53 -0400

    Update post_reboot.sh
    added exit on error, and added -p for mkdirs

--- post_reboot.sh
+++ post_reboot.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 cd ~/builds
 sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R mb:mb ./yay-git
@@ -7,9 +16,8 @@ makepkg -si
 sudo pacman -S pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server xterm --noconfirm
 sudo pacman -S feh bspwm sxhkd maim xclip picom ttf-font awesome rofi --noconfirm
 yay -S polybar xst --noconfirm
-mkdir ~/.config
-mkdir ~/.config/bspwm
-mkdir ~/.config/sxhkd
+mkdir -p ~/.config/bspwm
+mkdir -p ~/.config/sxhkd
 ln -sf ~/builds/bspwm_dotfiles/bspwmrc ~/.config/bspwm
 ln -sf ~/builds/bspwm_dotfiles/sxhkdrc ~/.config/sxhkd
 chmod +x ~/builds/bspwm_dotfiles/bspwmrc
@@ -19,9 +27,7 @@ ln -sf ~/builds/bspwm_dotfiles/rofi ~/.config/rofi
 ln -sf ~/builds/bspwm_dotfiles/Xresources ~/.Xresources
 cd ~/builds
 git clone https://github.com/siduck76/bspwm-dotfiles
-mkdir ~/.local
-mkdir ~/.local/share
-mkdir ~/.local/share/fonts
+mkdir -p ~/.local/share/fonts
 cp -r ~/builds/bspwm-dotfiles/fonts\!\ \(\ jetbrainsmono\ nerd\ font\ +\ material\ \) ~/.local/share/fonts
 cd ~/.local/share/fonts
 fc-cache -fv
@@ -33,3 +39,7 @@ sudo pacman -S zsh --noconfirm
 chsh -s /usr/bin/zsh
 sudo pacman -S papirus-icon-theme --noconfirm
 yay -S canta-gtk-theme --noconfirm
+
+# -mkdir -p makes the parent directories if necessary
+mkdir -p ~/Pictures/desktop\ backgrounds
+mv ~/builds/bspwm_dotfiles/botw.png ~/Pictures/desktop\ backgrounds
