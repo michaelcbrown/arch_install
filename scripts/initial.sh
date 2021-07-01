610aa798a0f69dc4a7ca32e04b6f3451151826a8
michaelcbrown <55893579+michaelcbrown@users.noreply.github.com>
2021-07-01 10:28:17 -0400

    Update initial.sh

--- scripts/initial.sh
+++ scripts/initial.sh
@@ -1,1 +1,28 @@
-temp
+#!/bin/bash
+
+set -e
+
+cfdisk
+mkfs.ext2 /dev/sda1
+mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
+pacstrap /mnt base linux linux-firmware base-devel nano sudo git iwd dhcpcd
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+
+sed sed -i ‘s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/’ /etc/locale.gen
+locale-gen
+echo “LANG=en_US.UTF-8” > /etc/locale.conf
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+echo “mb_arch” > /etc/hostname
+hwclock --systohc
+passwd
+useradd -m -G wheel mb
+passwd mb
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+pacman -S grub
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable dhcpcd
