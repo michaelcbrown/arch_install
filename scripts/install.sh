f19fda9ba3f0a9358fd5b4bdcc54e648441b743e
michaelcbrown <55893579+michaelcbrown@users.noreply.github.com>
2021-07-01 12:40:53 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -24,13 +24,16 @@ arch-chroot /mnt echo "LANG=en_US.UTF-8" > /etc/locale.conf
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 arch-chroot /mnt echo "mb_arch" > /etc/hostname
 arch-chroot /mnt hwclock --systohc
+arch-chroot /mnt passwd
 arch-chroot /mnt useradd -m -G wheel mb
+arch-chroot /mnt passwd mb
+#printf "#$USERPW\N$USERPW" | arch-chroot /mnt passwd mb
 arch-chroot /mnt sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 arch-chroot /mnt pacman -S grub
 arch-chroot /mnt grub-install /dev/sda
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 arch-chroot /mnt systemctl enable dhcpcd
 
-arch-chroot /mnt mkdir /home/mb/builds && cd /home/mb/builds
-arch-chroot /mnt git clone https://github.com/michaelcbrown/arch_install
-arch-chroot /mnt chmod +x /home/mb/builds/arch_install/scripts/first_login.sh
+curl -LJO https://raw.githubusercontent.com/michaelcbrown/arch_install/master/scripts/first_login.sh
+chmod +rwx first_login.sh
+mv first_login.sh /mnt/home/mb
