83aab0b325374f83e1ebf772a3a0f5c4f4088286
michaelcbrown <55893579+michaelcbrown@users.noreply.github.com>
2021-07-01 12:41:16 -0400

    Update first_login.sh

--- scripts/first_login.sh
+++ scripts/first_login.sh
@@ -12,8 +12,8 @@ REPO="/home/mb/builds/arch_install"
 BUILDS="/home/mb/builds"
 USERNAME="mb"
 
-#mkdir $BUILDS && cd $BUILDS
-#sudo git clone https://github.com/michaelcbrown/arch_install
+mkdir $BUILDS && cd $BUILDS
+sudo git clone https://github.com/michaelcbrown/arch_install
 
 # installing yay
 cd $BUILDS
