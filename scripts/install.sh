30d558f8f9679f38051ed962cc8cff1feebf0ce1
michaelcbrown <55893579+michaelcbrown@users.noreply.github.com>
2021-07-01 22:26:38 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -125,7 +125,7 @@ install_cinnamon () {
 
 main () {
     case $1 in
-        "partition")
+        partition)
             partition
             ;;
             
@@ -166,4 +166,4 @@ main () {
     shift
 }
 
-main
+main $1
